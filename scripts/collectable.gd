c2b8db82567c7fa159a39956025b156c23f8c2cb
Maxi RV <m.riquelme.vera@hotmail.com>
2024-08-16 23:24:10 -0300

    added collectable animated SpriteSheet + NOTWORKING

--- scripts/collectable.gd
+++ scripts/collectable.gd
@@ -4,14 +4,22 @@ extends Area2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var collision_shape_2d = $CollisionShape2D
 
+var readyToRemove: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+
+func _physics_process(delta):
+	if readyToRemove and animated_sprite_2d.animation_finished:
+		animated_sprite_2d.visible = false
 
 func _on_body_entered(body):
 	# TODO: Call gamemanager!
-	animated_sprite_2d.visible = false
 	collision_shape_2d.disabled = true
+	animated_sprite_2d.play("break")
+	readyToRemove = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
