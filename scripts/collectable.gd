cff602763b86c887baf560d11b6696118b99c8f5
Maxi RV <m.riquelme.vera@hotmail.com>
2024-08-16 22:21:44 -0300

    collectables-wip

---
+++ scripts/collectable.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@onready var collectable = $"."
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var collision_shape_2d = $CollisionShape2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_body_entered(body):
+	# TODO: Call gamemanager!
+	animated_sprite_2d.visible = false
+	collision_shape_2d.disabled = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
