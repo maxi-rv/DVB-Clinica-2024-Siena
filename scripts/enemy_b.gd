a6e89a46b7ebc0e522ff12b7d58a9c05523bbffe
Maximiliano Riquelme Vera <53203441+maxi-rv@users.noreply.github.com>
2024-08-27 18:34:59 -0300

    musicChanges + swingingMine + icon (#29)
    * musicChanges + swingingMine + icon
    
    * swingingMine correction
    
    * moved swingingMines

---
+++ scripts/enemy_b.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+@onready var timer = $KillZone/Timer
+
+func _physics_process(delta):
+	pass
+
+func _on_kill_zone_body_entered(body):
+	if body.name == "Player":
+		body.isDead = true
+		body.get_node("CollisionShape2D").queue_free()
+		body.get_node("AnimatedSprite2D").play("hurt")	
+		timer.start()
+
+func _on_timer_timeout():
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
